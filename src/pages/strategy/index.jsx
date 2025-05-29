01cc5168b87660f21c50c1c7953311aadcc7abea
muskan-67 <guptamuskan1267@gmail.com>
2025-05-29 21:09:35 +0530

    strategy

--- src/pages/strategy/index.jsx
+++ src/pages/strategy/index.jsx
@@ -1,10 +1,13 @@
-const strategy = () => { 
-    return (
-        <div>
-        <h1>Strategy</h1>
-        <p>Welcome to the Strategy page!</p>
-        </div>
-    );
-}
 
-export default strategy;
+import React from "react";
+
+const Strategy = () => {
+  return (
+    <div>
+      <h2>Strategy Page</h2>
+      <p>This is the Strategy page.</p>
+    </div>
+  );
+};
+
+export default Strategy;
