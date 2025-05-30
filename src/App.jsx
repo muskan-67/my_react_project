11db70aa2559b1e6ec9701b55b53d914cfca6f51
muskan-67 <guptamuskan1267@gmail.com>
2025-05-30 20:17:18 +0530

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Strategy from "./pages/Strategy";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
 import Portfolio from "./pages/Portfolio";
 import Report from "./pages/Report";
 import Broking from "./pages/Broking";
